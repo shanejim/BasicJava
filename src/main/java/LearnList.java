e114c9c431936354197dd70ada6f0561c18c2eaf
PanShenjia <ShaneJim@users.noreply.github.com>
2018-10-30 22:44:35 +0800

    test list

--- src/main/java/LearnList.java
+++ src/main/java/LearnList.java
@@ -1,7 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author shanejim
  * @description todo
  * @date 2018/10/25
  */
 public class LearnList {
+
+    public static void main(String args[]) {
+
+        List<String> strList = new ArrayList<String>();
+
+
+        System.out.println("Hello World!");
+    }
 }
