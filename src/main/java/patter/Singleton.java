59bd382101ef2e3f0561e73fa94fce389400518c
PanShenjia <ShaneJim@users.noreply.github.com>
2018-11-02 15:07:50 +0800

    单例

---
+++ src/main/java/patter/Singleton.java
@@ -0,0 +1,21 @@
+package patter;
+
+/**
+ * @description: 单例模式
+ * @author: panshenjia
+ * @create: 2018-11-02 15:06
+ **/
+public class Singleton {
+
+    private Singleton(){}
+
+    public static Singleton getInstance(){
+        return SingletonInstance.instance;
+    }
+
+    private static class SingletonInstance{
+
+        static Singleton instance = new Singleton();
+
+    }
+}
