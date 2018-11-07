f2104b79d79e875b5b9b7e452ec1201128f24645
PanShenjia <ShaneJim@users.noreply.github.com>
2018-11-07 16:11:19 +0800

    i++  ++i的区别

---
+++ src/main/java/basic/Basic.java
@@ -0,0 +1,17 @@
+package basic;
+
+/**
+ * @description: basic grammar
+ * @author: shanejim
+ * @create: 2018-11-07 16:06
+ **/
+public class Basic {
+    public static void main(String args[]) {
+        int i = 1, j = 1;
+        int a = i++;
+        int b = ++j;
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+}
