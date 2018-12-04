849c07d8881de9fd882f192ca1b8d1a618da89d2
PanShenjia <ShaneJim@users.noreply.github.com>
2018-12-04 23:58:57 +0800

    多线程

---
+++ src/main/java/multithread/JavaThreadCreationAndRun.java
@@ -0,0 +1,37 @@
+package multithread;
+
+import java.util.concurrent.BlockingQueue;
+
+/**
+ * @author shanejim
+ * @description todo
+ * @date 2018/12/4
+ */
+public class JavaThreadCreationAndRun {
+
+    public static void main(String[] args) {
+        Helper helper = new Helper("Java Thread Anywhere");
+
+        Thread thread = new Thread(helper);
+
+        thread.setName("A-Worker-Thread");
+        thread.getState();
+        thread.start();
+    }
+
+    static class Helper implements Runnable {
+        private final String message;
+
+        public Helper(String message) {
+            this.message = message;
+        }
+
+        private void doSomething(String message) {
+            System.out.print(Thread.currentThread().getName());
+        }
+
+        public void run() {
+            doSomething(message);
+        }
+    }
+}
