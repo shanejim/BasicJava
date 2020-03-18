7f6049b577b68d3f5d8e445289064c12fb921eae
ShaneJim <1693831790@qq.com>
2020-03-18 09:42:10 +0800

    mutli thread

---
+++ src/main/java/com/shanejim/javabasis/multithread/MyThread.java
@@ -0,0 +1,15 @@
+package com.shanejim.javabasis.multithread;
+
+public class MyThread extends Thread {
+    public MyThread(String name) {
+        super(name);
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            //输出当前线程的名称
+            System.out.println(Thread.currentThread().getName() + i);
+        }
+    }
+}
