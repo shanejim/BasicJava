4d7619f46d48e6f65cc27f54511b839eab9a2159
ShaneJim <1693831790@qq.com>
2020-03-25 22:06:55 +0800

    update

--- src/main/java/com/shanejim/javabasis/lang/reflect/LearnReflect.java
+++ src/main/java/com/shanejim/javabasis/lang/reflect/LearnReflect.java
@@ -1,5 +1,6 @@
 package com.shanejim.javabasis.lang.reflect;
 
+//反射：运行时生成对象实例，运行期间调用方法，运行时更改属性
 //通过反射创建对象性能更低，程序需要动态创建某个类的对象时才考虑使用反射。比如从配置文件中读取只是某个类的字符串类名
 //Class newInstance,Constructor
 //Field
